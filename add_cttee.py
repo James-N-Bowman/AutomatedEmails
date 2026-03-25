a0493e1040d392d7fa806edcce516b8154d5ab46
James-N-Bowman <james.n.bowman@gmail.com>
2026-03-25 11:30:26 +0000

    Altered add_cttee.py back to adding single committees at a time. Used it to add more committees to the mapping.csv file and Mailchimp.

--- add_cttee.py
+++ add_cttee.py
@@ -7,27 +7,36 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 def main():
-    # cttee_id = input("Committee id?").strip()
-    # try:
-    #     cttee_id = int(cttee_id)
-    # except ValueError as e:
-    #     logger.debug(f"Error: must enter integer value: {e}")
-    #     return
+    cttee_id = input("Committee id?").strip()
+    try:
+        cttee_id = int(cttee_id)
+    except ValueError as e:
+        logger.debug(f"Error: must enter integer value: {e}")
+        return
     
-    cttees = fetch_committees_dict('Select', '(HC) Public Standing Orders - Departmental', False)
+    cttees = fetch_committees_dict('Select', 
+        ['(HC) Public Standing Orders - Cross-cutting', 
+         '(HC) Public Standing Orders - Departmental',
+         '(HC) Public Standing Orders - Domestic'
+        ], 
+        False
+    )
 
-    # if cttee_id in cttees:
-    #     cttee_name = cttees[cttee_id]['name']
-    #     logger.info(f"{cttee_name} found on committees API.")
-    # else:
-    #     logger.warning(f"{cttee_name} found on committees API.")
-    #     return
-    
-    for cttee_id, cttee_value in cttees.items():
-        cttee_name = cttee_value['name']
+    if cttee_id in cttees:
+        cttee_name = cttees[cttee_id]['name']
+        logger.info(f"{cttee_name} found on committees API.")
         interest = create_group_interest(cttee_name)
         interest_id = interest.get("id")
         helpers_mapping_csv.update_mapping_CSV(cttee_id, cttee_name, interest_id) #campaign_id, interest_id)
+    else:
+        logger.warning(f"{cttee_name} found on committees API.")
+        return
+    
+    # for cttee_id, cttee_value in cttees.items():
+    #     cttee_name = cttee_value['name']
+    #     interest = create_group_interest(cttee_name)
+    #     interest_id = interest.get("id")
+    #     helpers_mapping_csv.update_mapping_CSV(cttee_id, cttee_name, interest_id) #campaign_id, interest_id)
 
 if __name__ == "__main__":
     main()
